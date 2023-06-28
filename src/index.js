c3aec877b917874ce30fa807863f76f272fe92c4
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-06-28 21:57:36 +0300

    deleted unnecessary code

--- src/index.js
+++ src/index.js
@@ -81,28 +81,3 @@ async function loadMore() {
   }
 }
 
-function markupGalleryList(arr) {
-  const markup = arr
-    .map(
-      item => `<div class="photo-card">
-  <a href="${item.largeImageURL}"><img class="photo" src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="350px" height="200px" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${item.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${item.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${item.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${item.downloads}</b>
-    </p>
-  </div>
-</div>`
-    )
-    .join('');
-
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
-}
